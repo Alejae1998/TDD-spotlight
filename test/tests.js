580c387c993775a53de2cdc13e06e7676169fd0e
Michelle Ross <miross629@gmail.com>
2022-11-03 12:55:43 -0700

    Remove extra code

--- test/tests.js
+++ test/tests.js
@@ -16,18 +16,16 @@ const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
     const expected = 'hello michelle';
-
     const actual = myFunction('michelle');
-
-    expect.equal(actual, expected, 'michelle');
+    expect.equal(actual, expected);
 
     const expected2 = 'hello alejandra';
     const actual2 = myFunction('alejandra');
-    expect.equal(actual2, expected2, 'alejandra');
+    expect.equal(actual2, expected2);
 
     const expected3 = 'hello ALEJANDRA AND MICHELLE';
     const actual3 = myFunction('ALEJANDRA AND MICHELLE');
-    expect.equal(actual3, expected3, 'ALEJANDRA AND MICHELLE');
+    expect.equal(actual3, expected3);
 });
 
 skip('this test should be skipped', (expect) => {
@@ -41,15 +39,15 @@ skip('this test should be skipped', (expect) => {
 test('this test should return a new string that adds three exclamation points', (expect) => {
     const expected = 'bunny rabbit!!!';
     const actual = addExclamationPoints('bunny rabbit');
-    expect.equal(actual, expected, 'bunny rabbit!!!');
+    expect.equal(actual, expected);
 
     const expected2 = 'alejandra!!!';
     const actual2 = addExclamationPoints('alejandra');
-    expect.equal(actual2, expected2, 'alejandra!!!');
+    expect.equal(actual2, expected2);
 
     const expected3 = 'michelle!!!';
     const actual3 = addExclamationPoints('michelle');
-    expect.equal(actual3, expected3, 'michelle!!!');
+    expect.equal(actual3, expected3);
 });
 
 test('this test should take a number and multiply by seven', (expect) => {
