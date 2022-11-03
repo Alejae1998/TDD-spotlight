78ea6ccb881a6dc206e550c78190b516c3cd2aa2
Alejae1998 <alejagr1998@gmail.com>
2022-11-03 11:08:20 -0700

    failing myFunction test

--- test/tests.js
+++ test/tests.js
@@ -4,11 +4,11 @@ import { myFunction } from '../functions.js';
 const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
-    const expected = true;
+    const expected = 'bunny rabbit!!!';
 
-    const actual = myFunction();
+    const actual = myFunction('bunny rabbit');
 
-    expect.equal(actual, expected, 'true = true');
+    expect.equal(actual, expected, 'bunny rabbit');
 });
 
 skip('this test should be skipped', (expect) => {
